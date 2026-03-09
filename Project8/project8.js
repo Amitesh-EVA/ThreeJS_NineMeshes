d96110f0387a2efc72974ade9ded494959943490
Amitesh Sahu <amiteshsahu@evawinoptimize.com>
2026-03-09 10:05:00 +0530

    Project8 UI Updated

--- Project8/project8.js
+++ Project8/project8.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
+
 const scene= new THREE.Scene();
-scene.background= new THREE.Color('#ffffff');
 
 const camera= new THREE.PerspectiveCamera(
     75,
@@ -10,7 +10,6 @@ const camera= new THREE.PerspectiveCamera(
     0.01,
     10000
 );
-
 camera.position.z= 300;
 
 const renderer= new THREE.WebGLRenderer({antialias:true});
@@ -21,26 +20,18 @@ const controls= new OrbitControls(camera,renderer.domElement);
 controls.enableDamping=true;
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
-// light.position.set(50, 50, 100);
-scene.add(light);
-
+scene.add(light)
 const ambient = new THREE.AmbientLight(0xffffff, 3);
 scene.add(ambient);
+// const axesHelper= new THREE.AxesHelper(100);
+// scene.add(axesHelper);
 
-const axesHelper= new THREE.AxesHelper(100);
-scene.add(axesHelper);
-
-const h=200;
-const w=80;
-const x=10;
-const depth=10;
-const D=15;
-const d=10;
-const h1=h/8-x/2;
-
-
-
-
+let h=250;
+let w=80;
+let x=10;
+let depth=10;
+let D=15;
+let d=10;
 
 function createParent(){
     const shape=new THREE.Shape();
@@ -59,6 +50,7 @@ function createParent(){
 }
 
 function createFirstChild(){
+    const h1=h/8-x/2;
     const shape=new THREE.Shape();
     shape.moveTo(0,0);
     shape.lineTo(w-x,0);
@@ -68,26 +60,21 @@ function createFirstChild(){
 
     shape.holes.push(createHole((w-x)/4, h1, d/2));
     shape.holes.push(createHole(3*(w-x)/4, h1, d/2));
-
     return shape;
 }
 
 function createSecondChild(){
-
     const w1 = w/2;
     const shape = new THREE.Shape();
-
     shape.moveTo(w1/2,0);
     shape.absarc(w1/2,0,w1/2,Math.PI,0,false);
     shape.lineTo(w1,h/2);
-    shape.lineTo(-0.4*w1,h/2);
+    shape.lineTo(-w1/2,h/2);
     shape.lineTo(0,h/2 - h*0.05);
     shape.lineTo(0,0);
 
     shape.holes.push(createHole(w1/2,0,D/2));
-
     return shape;
-
 }
 
 function createHole(x,y,radius){
@@ -106,15 +93,14 @@ let extrudedSettings={
 const geometry1= new THREE.ExtrudeGeometry(shape1,extrudedSettings);
 const geometry2= new THREE.ExtrudeGeometry(shape2,extrudedSettings);
 const geometry3= new THREE.ExtrudeGeometry(shape3,extrudedSettings);
-
-// console.log(geometry);
+geometry3.translate(-w/4,0,0);
 
 const material1= new THREE.MeshStandardMaterial({
     color:'#D8BBA9',
     metalness:0.2
 })
 const material2= new THREE.MeshStandardMaterial({
-    color:'black',
+    color:'grey',
     metalness:0.6,
     roughness:0.4,
 })
@@ -123,7 +109,8 @@ const material3= new THREE.MeshStandardMaterial({
     metalness:0.6,
     roughness:0.4,
 
-}) 
+
+})
 
 const parent= new THREE.Mesh(geometry1,material1);
 const child1= new THREE.Mesh(geometry2,material2);
@@ -132,15 +119,48 @@ const child2= new THREE.Mesh(geometry3,material3);
 child1.position.set(x, h/4 + x/2, depth);
 
 child2.rotation.z = -50 * Math.PI / 180;
-child2.position.set(
-    w/4 + x/2,
-    -3*h/16,
-    depth
-);
+child2.position.set((w-x)/2 + x, -h/8, depth);
 
 scene.add(parent);
 parent.add(child1);
 parent.add(child2);
+
+function rebuildGeometry(){
+    parent.geometry.dispose();
+    child1.geometry.dispose();
+    child2.geometry.dispose();
+
+    extrudedSettings.depth = depth;
+
+    parent.geometry = new THREE.ExtrudeGeometry(createParent(),extrudedSettings);
+    child1.geometry = new THREE.ExtrudeGeometry(createFirstChild(),extrudedSettings);
+    child2.geometry = new THREE.ExtrudeGeometry(createSecondChild(),extrudedSettings);
+    child2.geometry.translate(-(w*0.45)/2,0,0);
+
+    child1.position.set(x , h/4 + x/2 , depth);
+
+    child2.rotation.z = -50 * Math.PI / 180;
+    child2.position.set(
+    (w-x)/2 + x ,
+    -h/8 ,
+    depth
+    );
+
+    scene.add(parent);
+    parent.add(child1);
+    parent.add(child2);
+}
+
+document.getElementById("updateMesh").addEventListener("click",()=>{
+h = Number(document.getElementById("height").value);
+w = Number(document.getElementById("width").value);
+x = Number(document.getElementById("offset").value);
+depth = Number(document.getElementById("depth").value);
+D = Number(document.getElementById("bigHole").value);
+d = Number(document.getElementById("smallHole").value);
+
+rebuildGeometry();
+});
 
 function animate()
 {
